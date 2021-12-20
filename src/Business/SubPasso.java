a032ce7e0375fcf813ee25a15f7db0a8f2e86072
Anaa10 <a93284@alunos.uminho.pt>
2021-12-20 18:59:27 +0000

    classe SubPasso adicionada

--- src/Business/SubPasso.java
+++ src/Business/SubPasso.java
@@ -1,2 +1,24 @@
-package Business;public class SubPasso {
-}
+package Business;
+
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+public class SubPasso extends Passo{
+
+    public SubPasso() {
+        super();
+    }
+
+    public SubPasso(float custoPasso, LocalTime tempoPasso){
+        super(custoPasso,tempoPasso);
+    }
+
+    public SubPasso(SubPasso subPasso){
+        super(subPasso);
+    }
+/*
+    public SubPasso clone(){
+        return new SubPasso(this);
+    }
+ */
+    }
