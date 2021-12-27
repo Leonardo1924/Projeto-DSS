1ebbeb5094f5b5ebc669cdfe7d582c77958abdb7
Ana Henriques <pbapohenriques@hotmail.com>
2021-12-27 12:39:31 +0000

    debug

---
+++ src/Business/Store/Cliente/IGestClientes.java
@@ -0,0 +1,19 @@
+package Business.Store.Cliente;
+
+import java.util.List;
+import java.util.Map;
+
+public interface IGestClientes {
+
+    List<String> getContactados();
+
+    List<String> getNaoContactados();
+
+    Map<String,Cliente> getClientes();
+
+    void removeCliente(String id);
+
+    void registaCliente(String idCliente, String nome, int nif, int telemovel, String mail, String idEquip);
+
+    void consultaClientes();
+}
