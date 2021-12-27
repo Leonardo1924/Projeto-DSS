017229943f63b99d163f90ece35a9e8ae1b61d15
Ana Henriques <pbapohenriques@hotmail.com>
2021-12-27 17:33:34 +0000

    classe Servico atualizada

--- src/Business/Store/Servico/Servico.java
+++ src/Business/Store/Servico/Servico.java
@@ -1,24 +1,30 @@
 package Business.Store.Servico;
 
+import Business.Store.Cliente.Cliente;
+
 public class Servico {
     private String id;
+    private String tipo;       // expresso ; normal
     private float custoTotal;
-    private int tempoTotal;
+    private float tempoTotal;
 
     public Servico() {
         this.id = "";
+        this.tipo = "";
         this.custoTotal = 0;
         this.tempoTotal = 0;
     }
 
-    public Servico(String id, float custoTotal, int tempoTotal) {
+    public Servico(String id, String tipo, float custoTotal, float tempoTotal) {
         this.id = id;
+        this.tipo = tipo;
         this.custoTotal = custoTotal;
         this.tempoTotal = tempoTotal;
     }
 
     public Servico(Servico servico) {
         this.id = servico.getId();
+        this.tipo = servico.getTipo();
         this.custoTotal = servico.getCustoTotal();
         this.tempoTotal = servico.getTempoTotal();
     }
@@ -27,31 +33,36 @@ public class Servico {
         return this.id;
     }
 
+    public String getTipo() {
+        return this.tipo;
+    }
+
     public float getCustoTotal() {
         return this.custoTotal;
     }
 
-    public int getTempoTotal() {
+    public float getTempoTotal() {
         return this.tempoTotal;
     }
-/*
+
     public Servico clone(){
         return new Servico(this);
     }
 
     public String toString(){
-        return "Serviço{" +
-                "Id: " + this.id +
-                "Custo Total: " + this.custoTotal +
-                "Tempo Total: " + this.tempoTotal +
-                '}';
+        return "\033[1;35mId: \033[0m" + this.id +
+                " \033[1;35mTipo: \033[0m" + this.tipo +
+                " \033[1;35mCusto Total: \033[0m" + this.custoTotal +
+                " \033[1;35mTempo Total: \033[0m" + this.tempoTotal;
     }
 
     public boolean equals(Object obj){
         if (obj == this) return true;
         if (obj == null || obj.getClass().equals(this.getClass())) return false;
-        Equipamento equipamento = (Equipamento) obj;
-        return this.id.equals(equipamento.getId());
+        Servico servico = (Servico) obj;
+        return this.id.equals(servico.getId()) &&
+                this.tipo.equals(servico.getTipo()) &&
+                this.custoTotal == servico.getCustoTotal() &&
+                this.tempoTotal == servico.getTempoTotal();
     }
-    */
 }
